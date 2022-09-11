909aea6d14cab938195f8617d32f467f3d6d80ee
MohammedNBM <mohammednuru.BM@gmail.com>
2022-09-11 00:40:14 -0700

    printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Return : alays 0 9success0
+ */
+    int main(void)
+{
+	printf("Programming is like building a multilingual puzzle")
+		return (0);
+}
+
