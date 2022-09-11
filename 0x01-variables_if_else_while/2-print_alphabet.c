dfbd20d367605620370097d5d1697fdee938e1ba
MohammedNBM <mohammednuru.BM@gmail.com>
2022-09-11 02:55:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entery point
+ *
+ * Return: always (0) suces
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+		return (0);
+}
