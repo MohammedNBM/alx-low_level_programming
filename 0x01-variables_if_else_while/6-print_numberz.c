7a70b6a318b1b393ee4ded410cf959ee2856cbb8
MohammedNBM <mohammednuru.BM@gmail.com>
2022-09-12 13:55:30 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entery pont
+ *
+ * Return: always (0) sucess
+ */
+int main(void)
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
