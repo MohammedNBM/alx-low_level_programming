dfa199685bfedbfe593cd6e9340ef196d9e90893
MohammedNBM <mohammednuru.BM@gmail.com>
2022-09-12 14:09:34 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entery point 
+ *
+ * Return: always (0) success
+ */
+nt main(void)
+{
+	char low;
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for( low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
