fc17e3fc32fac7c75a71d464fa0417ebad245c30
mohammedNBM <mohammednuru.BM@gmail.com>
2022-09-20 15:07:00 -0700

    revr

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,17 @@
  */
 void rev_string(char *s)
 {
-	int len = 0, i = 0;
-	char aux;
+	int length = 0, i, length2 = 0;
+	char tmp;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len--)
+	while (s[length] != '\0')
+		length++;
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
-		aux = s[len];
-		s[i++] = s[len];
-		s[len] = aux;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	}
 }
