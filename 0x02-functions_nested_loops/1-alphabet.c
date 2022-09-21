73bc860e0fce0e31386ac46cc7829f9d3f384243
mohammedNBM <mohammednuru.BM@gmail.com>
2022-09-21 07:54:38 -0700

    printing lphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet - print alphabet
+ *
+ *Retrun: always success
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
