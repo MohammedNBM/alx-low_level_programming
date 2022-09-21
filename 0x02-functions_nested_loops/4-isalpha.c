dbcd03524f7dd319fa1ea0d893b70f4c689ff771
mohammedNBM <mohammednuru.BM@gmail.com>
2022-09-21 08:34:33 -0700

    isaplha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_isalpha -  checks if c is alph
+ *@c: to be cheked
+ *Return: returns 1 and 0 based to condition
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c >= 'z');
+}
