3c7d7aa459e35f6e0959a83bf0e1a8eb3acfdf6f
mohammedNBM <mohammmednuru.BM@gmail.com>
2022-09-22 01:39:24 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - check desription
+ *@a: value to be sum
+ *@b: value
+ *Return: sum of two parameters
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
