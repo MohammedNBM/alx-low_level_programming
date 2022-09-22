06f6bdab1b403fcf203e85fbea343638f6985e55
mohammedNBM <mohammmednuru.BM@gmail.com>
2022-09-22 00:41:10 -0700

    absulute val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - check absulte value
+ *@r: parameter to bchecked
+ *Return: r values
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
