37aae0d90d8edb7b2d4a718c9d7d0ff373b9a709
mohammedNBM <mohammednuru.bm@gmail.com>
2022-09-24 01:41:27 -0700

    printing num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers -  a function prints a number from 0 to 9
+ *
+ *Return: always success
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
