4171615035174b3b898429846b46a5663aea9915
mohammedNBM <mohammednuru.bm@gmail.com>
2022-09-24 04:11:05 -0700

    digonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_diagonal - a function that draws digonal line
+ *@n: parameter to be printed
+ *Return: always success
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('98');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
